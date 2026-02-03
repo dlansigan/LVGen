92937e090929774ba2bd1f101cde1a5a686129fa
dlansigan <dlavacot@wustl.edu>
2026-02-03 16:05:50 -0600

    Add script for generating volume mesh and face meshes

--- vis_LVs.py
+++ vis_LVs.py
@@ -16,9 +16,11 @@ if __name__=="__main__":
     meshes = {}
     for phase in range(n_phases):
         for n in range(n_meshes):
+            # Load mesh at phase
             mesh_dir = "mesh_{}".format(str(n))
             fn = os.path.join(template_dir,mesh_dir,"phase{}.vtp".format(str(phase)))
 
+            # Plot
             row, col = np.unravel_index(n,(n_rows,n_cols))
             pl.subplot(row, col)
             if phase==0:
@@ -26,15 +28,16 @@ if __name__=="__main__":
                 pl.add_mesh(meshes[str(n)],scalars='ModelFaceID',
                             opacity=0.75,
                             show_scalar_bar=False)
-                pl.add_title(mesh_dir,font_size=10)
-                pl.camera.azimuth=135
-                pl.camera.elevation=-90
+                # pl.add_title(mesh_dir,font_size=10)
+                pl.camera.azimuth=-90
+                pl.camera.elevation=30
+                pl.camera.roll=-60
+                pl.camera.zoom(1.5)
             else:
                 new_mesh = pv.read(fn)
                 meshes[str(n)].points = new_mesh.points
             
         pl.write_frame()
     pl.link_views()
-    pl.show_axes()
     # pl.show()
     pl.close()
