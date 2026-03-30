6a6117ccd1216c02ef7d3d5d5f1cf48fab9f886f
dlansigan <dlavacot@wustl.edu>
2026-03-30 15:36:50 -0500

    Add base case and bash scripts for preparing and running multiple cases

---
+++ workflow_setup_batch.sh
@@ -0,0 +1,45 @@
+# This script sets up the meshes for the different geometry cases.
+# Run simulations using workflow_run_batch.sh.
+
+start=$1
+end=$2
+phase=9
+
+# Rescale meshes first
+python rescale_mesh.py \
+        --template_dir generated_meshes \
+        --save_dir scaled_meshes \
+        --n_meshes 10 \
+        --scale 30
+
+for case in $(seq $start $end); do
+        case_dir=cases/LV_$case
+
+        ### GENERATE MESHES ###
+
+        # Clean meshes directory
+        rm -r $case_dir/mesh
+        mkdir -p $case_dir/mesh
+
+        # Mesh one case
+        simvascular --python -- sv_mesh.py \
+                --template_dir scaled_meshes/ \
+                --case $case_dir \
+                --mesh_id $case\
+                --phase $phase \
+                --quality 1.0\
+                --edge_size 0.5\
+                --overwrite > $case_dir/mesh/mesh.log
+        python interpolate_meshes.py \
+                --input_dir scaled_meshes/mesh_${case}/ \
+                --output_dir $case_dir/mesh/motion/ \
+                --mesh_complete_surface $case_dir/mesh/mesh_complete_surface.vtp\
+                --num_interpolation 99\
+                --num_cycle 1 \
+                --duration 0.8 \
+                --phase $phase
+        python save_init_p.py --case $case_dir --init_p 13332.2 # dyn/cm2
+
+        # Get param files
+        cp cases/base/* $case_dir
+done
